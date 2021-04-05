a44dcb23f4914b3768732b752bdc0e960605aef6
Esteban <esteban0477@gmail.com>
2021-04-04 22:36:56 -0500

    fix up

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,33 +12,35 @@ pipeline {
         }
       }
 
-      if (env.BRANCH_NAME == 'main'){
-        
-        def MODULES_TO_BUILD = sh script:"./detect_tf_changes.sh", returnStdout: true
-        MODULES_TO_BUILD.split("\n").each {
+      stage('Deploy cfn') {
+        if (env.BRANCH_NAME == 'main'){
           
-        stage('Create ChangeSet') {
-            steps {
-              withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'awsjuan', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                sh 'aws cloudformation validate-template --template-body file://${MODULES_TO_BUILD}/cfn_stack_spec.yaml'
-                sh 'cat ${MODULES_TO_BUILD}/parameters.yaml | yq eval -PMj > ${MODULES_TO_BUILD}/parameters.json'
-                sh 'aws cloudformation create-stack --stack-name ${MODULES_TO_BUILD} --template-body file://${MODULES_TO_BUILD}/cfn_stack_spec.yaml --parameters file://${MODULES_TO_BUILD}/parameters.json'
-              }
-            }      
+          def MODULES_TO_BUILD = sh script:"./detect_tf_changes.sh", returnStdout: true
+          MODULES_TO_BUILD.split("\n").each {
+            
+          stage('Create ChangeSet') {
+              steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'awsjuan', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                  sh 'aws cloudformation validate-template --template-body file://${MODULES_TO_BUILD}/cfn_stack_spec.yaml'
+                  sh 'cat ${MODULES_TO_BUILD}/parameters.yaml | yq eval -PMj > ${MODULES_TO_BUILD}/parameters.json'
+                  sh 'aws cloudformation create-stack --stack-name ${MODULES_TO_BUILD} --template-body file://${MODULES_TO_BUILD}/cfn_stack_spec.yaml --parameters file://${MODULES_TO_BUILD}/parameters.json'
+                }
+              }      
+            }
           }
-        }
 
-      } else {
-        
-        def MODULES_TO_BUILD = sh script:"./detect_tf_changes.sh", returnStdout: true
-        MODULES_TO_BUILD.split("\n").each {
+        } else {
           
-        stage('Validate template') {
-            steps {
-              withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'awsjuan', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                sh 'aws cloudformation validate-template --template-body file://${MODULES_TO_BUILD}/cfn_stack_spec.yaml'
-              }
-            }      
+          def MODULES_TO_BUILD = sh script:"./detect_tf_changes.sh", returnStdout: true
+          MODULES_TO_BUILD.split("\n").each {
+            
+          stage('Validate template') {
+              steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'awsjuan', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                  sh 'aws cloudformation validate-template --template-body file://${MODULES_TO_BUILD}/cfn_stack_spec.yaml'
+                }
+              }      
+            }
           }
         }
       }
