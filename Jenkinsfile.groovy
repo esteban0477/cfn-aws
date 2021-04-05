5ff31419dc8bef167e76f4fc1e4150a5d2a81b1b
Esteban <esteban0477@gmail.com>
2021-04-05 00:02:52 -0500

    Testing with a folder

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,10 +19,10 @@ pipeline {
             def MODULES_TO_BUILD = sh script:"./detect_tf_changes.sh", returnStdout: true
             MODULES_TO_BUILD.split("\n").each {
               withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'awsjuan', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                sh 'ls '+MODULES_TO_BUILD
-                sh '''aws cloudformation validate-template --template-body file://'''+MODULES_TO_BUILD+'''cfn_stack_spec.yaml'''
-                sh '''cat '''+MODULES_TO_BUILD+'''parameters.yaml | yq eval -PMj > '''+MODULES_TO_BUILD+'''parameters.json'''
-                sh '''aws cloudformation create-stack --stack-name '''+MODULES_TO_BUILD+''' --template-body file://'''+MODULES_TO_BUILD+'''cfn_stack_spec.yaml --parameters file://'''+MODULES_TO_BUILD+'''parameters.json'''
+                sh "ls $MODULES_TO_BUILD"
+                //sh '''aws cloudformation validate-template --template-body file://'''+MODULES_TO_BUILD+'''cfn_stack_spec.yaml'''
+                //sh '''cat '''+MODULES_TO_BUILD+'''parameters.yaml | yq eval -PMj > '''+MODULES_TO_BUILD+'''parameters.json'''
+                //sh '''aws cloudformation create-stack --stack-name '''+MODULES_TO_BUILD+''' --template-body file://'''+MODULES_TO_BUILD+'''cfn_stack_spec.yaml --parameters file://'''+MODULES_TO_BUILD+'''parameters.json'''
               }
             }
           }    
