6190b3694fa00ac3f6cd1fbc4a57f5a36fbca173
Esteban <esteban0477@gmail.com>
2021-04-04 23:50:30 -0500

    Testing with a folder

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
             def MODULES_TO_BUILD = sh script:"./detect_tf_changes.sh", returnStdout: true
             MODULES_TO_BUILD.split("\n").each {
               withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'awsjuan', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                sh 'echo ${MODULES_TO_BUILD}'
+                sh 'ls '+MODULES_TO_BUILD
                 //sh 'aws cloudformation validate-template --template-body file://$MODULES_TO_BUILD/cfn_stack_spec.yaml'
                 //sh 'cat ${MODULES_TO_BUILD}/parameters.yaml | yq eval -PMj > $MODULES_TO_BUILD/parameters.json'
                 //sh 'aws cloudformation create-stack --stack-name $MODULES_TO_BUILD --template-body file://$MODULES_TO_BUILD/cfn_stack_spec.yaml --parameters file://$MODULES_TO_BUILD/parameters.json'
