0ad30b2497b94af38c3bc4c5acfb75331298535b
Esteban <esteban0477@gmail.com>
2021-04-05 12:18:47 -0500

    Testing with a folder

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,6 +11,7 @@ pipeline {
           git credentialsId: 'esteban0477', url: 'https://github.com/esteban0477/cfn-aws'
           sh 'chmod +x detect_tf_changes.sh'
           sh 'chmod +x get_stack_name.sh'
+          sh 'chmod +x validate_existence.sh'
         }
       }
 
@@ -24,7 +25,7 @@ pipeline {
                   sh 'pwd'
                   sh 'aws cloudformation validate-template --template-body file://cfn_stack_spec.yaml'
                   def STACK_NAME = sh script:"../get_stack_name.sh ${it}", returnStdout: true
-                  def STACK_NAME_EXISTS = sh script:"aws cloudformation list-stacks --stack-status-filter REVIEW_IN_PROGRESS CREATE_FAILED UPDATE_ROLLBACK_FAILED UPDATE_ROLLBACK_IN_PROGRESS CREATE_IN_PROGRESS IMPORT_ROLLBACK_IN_PROGRESS UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS ROLLBACK_IN_PROGRESS IMPORT_IN_PROGRESS UPDATE_COMPLETE UPDATE_IN_PROGRESS DELETE_FAILED IMPORT_COMPLETE DELETE_IN_PROGRESS ROLLBACK_COMPLETE ROLLBACK_FAILED IMPORT_ROLLBACK_COMPLETE UPDATE_COMPLETE_CLEANUP_IN_PROGRESS CREATE_COMPLETE IMPORT_ROLLBACK_FAILED UPDATE_ROLLBACK_COMPLETE | jq .StackSummaries[].StackName -r | grep -c $STACK_NAME", returnStdout: true
+                  def STACK_NAME_EXISTS = sh script:"../validate_existence.sh $STACK_NAME", returnStdout: true
                   /* if ("$STACK_NAME_EXISTS" == 0) {
                     sh 'echo "Stack does not exist"'
                     // sh 'aws cloudformation create-stack --template-body file://cfn_stack_spec.yaml --parameters file://parameters.json --stack-name '+STACK_NAME
